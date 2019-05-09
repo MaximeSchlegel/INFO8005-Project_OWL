07d09ca194c4cf3efa72309191714bef4799f56d
Mourad mamassi <mourad.mamassi@student.uliege.be>
2019-05-09 12:48:06 +0200

    add some function(addDeclarationAxiom(), addSubClassAxiom(), removeAxiom(),saveOntologyLocal())

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,29 @@
+import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.OWLException;
+import org.semanticweb.owlapi.model.OWLOntology;
 
+import java.io.FileNotFoundException;
 
 
 public class App {
-    public static void main(String[] args) throws OWLException {
+    public static void main(String[] args) throws OWLException, FileNotFoundException {
         Factory mFactory = new Factory();
-        mFactory.loadOntology("./JOV2.owl");
+
+        //Load OWL File
+        mFactory.loadOntologyLocal("src/main/resources/JOV2.owl");
+//        System.out.println(mFactory.getmOWLOntology());
+
+        //Modification Add un Axiom
+        mFactory.addDeclarationAxiom("test");
+
+        //Modificaton Add un Sub Axiom
+        mFactory.addSubClassAxiom("test", "subtest");
+
+        mFactory.removeAxiom("test");
+
+//        Save OWL File
+        mFactory.saveOntologyLocal("src/main/resources/JOVTEST2.owl");
+
 
     }
 }
--- src/main/java/Factory.java
+++ src/main/java/Factory.java
@@ -1,40 +1,85 @@
+import jdk.nashorn.internal.objects.annotations.Function;
 import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.formats.FunctionalSyntaxDocumentFormat;
 import org.semanticweb.owlapi.io.OWLOntologyDocumentSource;
 import org.semanticweb.owlapi.io.StreamDocumentSource;
 import org.semanticweb.owlapi.model.*;
+import org.semanticweb.owlapi.util.OWLClassLiteralCollector;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Factory {
+
+    private OWLManager mOWLManager;
+    private OWLOntology mOWLOntology;
     public void createOntology() throws OWLException {
-
-        OWLOntologyManager om = OWLManager.createOWLOntologyManager();
-        IRI pressinnov_iri = IRI.create("https://contentside.com/misc/exemple.owl");
-        OWLOntology pressInnovOntology = om.createOntology(pressinnov_iri);
-    }
-
-    public void loadOntology(String nameOfOntology) throws OWLException {
-
-        OWLOntologyManager om = OWLManager.createOWLOntologyManager();
-        OWLOntologyDocumentSource source = new StreamDocumentSource(getClass().
-                getResourceAsStream(nameOfOntology));
-        om.loadOntologyFromOntologyDocument(source);
-        OWLOntology pressInnovOntology = om.createOntology();
-    }
-
-    public void addClass() throws OWLException {
-
-        OWLOntologyManager om = OWLManager.createOWLOntologyManager();
-        IRI pressinnov_iri = IRI.create("https://contentside.com/misc/personne.owl");
-        OWLOntology pressinnovOntology = om.createOntology(pressinnov_iri);
-        OWLDataFactory factory = om.getOWLDataFactory();
-
-        OWLClass Sportif = factory.getOWLClass(IRI.create(pressinnov_iri + "#Sportif"));
-        OWLClass Footballeur = factory.getOWLClass(IRI.create(pressinnov_iri  + "#Footballeur"));
-
-        OWLAxiom axiom = factory.getOWLSubClassOfAxiom(Footballeur, Sportif);
-
-        AddAxiom addAxiom = new AddAxiom(pressinnovOntology, axiom);
-        om.applyChange(addAxiom);
+      OWLOntologyManager man = OWLManager.createOWLOntologyManager();
+      OWLOntology o = man.createOntology();
+      System.out.println(o);
 
     }
 
+    public void loadOntologyLocal(String nameOfOntology) throws OWLException {
+        OWLOntologyManager man = OWLManager.createOWLOntologyManager();
+        File file = new File(nameOfOntology);
+        this.mOWLOntology = man.loadOntologyFromOntologyDocument(file);
+    }
+
+    public void loadOntologyIRI(String IRIOfOntology) throws OWLException {
+        OWLOntologyManager man = OWLManager.createOWLOntologyManager();
+        IRI mIRI = IRI.create(IRIOfOntology);
+        this.mOWLOntology = man.loadOntology(mIRI);
+//        System.out.println(o);
+    }
+
+    public void saveOntologyIRI(String mIRI, String path) throws OWLOntologyCreationException, FileNotFoundException, OWLOntologyStorageException {
+        OWLOntologyManager man = OWLManager.createOWLOntologyManager();
+        File fileout = new File(path);
+        IRI ontologyIRI = IRI.create(mIRI);
+        this.mOWLOntology = man.loadOntology(ontologyIRI);
+        man.saveOntology(this.mOWLOntology, new FunctionalSyntaxDocumentFormat(), new FileOutputStream(fileout));
+    }
+
+    public void saveOntologyLocal(String path) throws FileNotFoundException, OWLOntologyStorageException {
+        OWLOntologyManager man = OWLManager.createOWLOntologyManager();
+        File fileout = new File(path);
+        man.saveOntology(this.mOWLOntology, new FunctionalSyntaxDocumentFormat(), new FileOutputStream(fileout));
+    }
+
+    public void addDeclarationAxiom(String mAxiom){
+        OWLDataFactory df = mOWLOntology.getOWLOntologyManager().getOWLDataFactory();
+        OWLClass axiom = df.getOWLClass("#" + mAxiom);
+        OWLDeclarationAxiom da = df.getOWLDeclarationAxiom(axiom);
+        mOWLOntology.add(da);
+    }
+    public void addSubClassAxiom(String mAxiom, String subAxiom){
+        OWLDataFactory df = mOWLOntology.getOWLOntologyManager().getOWLDataFactory();
+        OWLClass axiom = df.getOWLClass("#" + mAxiom);
+        OWLClass mSubAxiom = df.getOWLClass("#" + subAxiom);
+        OWLSubClassOfAxiom mOWLSubClassOfAxiom = df.getOWLSubClassOfAxiom(mSubAxiom, axiom);
+        mOWLOntology.add(mOWLSubClassOfAxiom);
+    }
+
+    public void removeAxiom(String mAxiom){
+        OWLDataFactory df = mOWLOntology.getOWLOntologyManager().getOWLDataFactory();
+        OWLClass axiom = df.getOWLClass("#" + mAxiom);
+        Set<OWLAxiom> axiomsToRemove = new HashSet<OWLAxiom>();
+        for (OWLAxiom ax : mOWLOntology.getAxioms()) {
+            if (ax.getSignature().contains(axiom)) {
+                axiomsToRemove.add(ax);
+            }
+        }
+        mOWLOntology.remove(axiomsToRemove);
+    }
+    public OWLManager getmOWLManager() {
+        return mOWLManager;
+    }
+
+    public OWLOntology getmOWLOntology() {
+        return mOWLOntology;
+    }
 }
