6b37ff75aee9c4ea2722174516e9f5869d70db1d
Mourad mamassi <mourad.mamassi@student.uliege.be>
2019-05-13 01:15:21 +0200

    A vous de voir ;)

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,44 +3,62 @@ import org.semanticweb.owlapi.model.OWLException;
 import org.semanticweb.owlapi.model.OWLOntology;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 
 public class App {
-    public static void main(String[] args) throws OWLException, FileNotFoundException {
+    public static void main(String[] args) throws OWLException, IOException {
         Factory mFactory = new Factory();
 
+        //Create OWL File
+        mFactory.createOntology();
+
         //Load OWL File
-        mFactory.loadOntologyLocal("src/main/resources/JOV4.owl");
+//        mFactory.loadOntologyLocal("src/main/resources/JOV4.owl");
 
         //Modification Add un Axiom
-        mFactory.addDeclarationAxiom("test");
+        mFactory.addDeclarationAxiom("Person");
+        mFactory.addDeclarationAxiom("University");
 
         //Modificaton Add un Sub Axiom
-        mFactory.addSubClassAxiom("test", "subtest");
+        mFactory.addSubClassAxiom("Person", "Woman");
+        mFactory.addSubClassAxiom("Person", "Man");
 
         //Remove Axiom
 //        mFactory.removeAxiom("test");
 
         //Add Property
-        mFactory.addProperty("hasTest", "test", "result");
+        mFactory.addProperty("isEnrolledIn", "Person", "University");
 
         //Add ObjectValueSome
-        mFactory.addComplexExpressionSome("test", "hasTest");
+        mFactory.addComplexExpressionSome("isEnrolledIn", "Person", "University");
 
         //Add ObjectValueExactlyCardinality
-        mFactory.addComplexExpressionExactCardinality("test", "hasTest", 1);
+        mFactory.addComplexExpressionExactCardinality("isEnrolledIn", "Person","University", 1);
 
         //Add Individual
-        mFactory.addIndividual("test", "Test1");
+        mFactory.addIndividual("Man", "Maxime");
+        mFactory.addIndividual("Man", "Fabien");
+        mFactory.addIndividual("Man", "Mourad");
+        mFactory.addIndividual("Woman", "Marine");
+
+        mFactory.addIndividual("University", "Uliege");
+
+        mFactory.addIndividualObjectProperty("Maxime", "isEnrolledIn", "Uliege");
 
         //Launch Reasoner
         mFactory.LaunchReasoner();
 
-        mFactory.inferredSubClass();
+//        mFactory.inferredSubClass();
+
         //Save OWL File
         mFactory.saveOntologyLocal("src/main/resources/JOVTEST4.owl");
-
+        //Show all Axioms
         mFactory.IterateOverAxioms();
+
+        //Execute some DL Query
+        //TODO Test DL Query : Person and isEnrolledIn University
+        mFactory.InitialiseQuery();
 
 
     }
--- src/main/java/Factory.java
+++ src/main/java/Factory.java
@@ -1,16 +1,21 @@
+import org.coode.owlapi.manchesterowlsyntax.ManchesterOWLSyntaxEditorParser;
 import org.semanticweb.HermiT.Reasoner;
 import org.semanticweb.HermiT.ReasonerFactory;
 import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.expression.OWLEntityChecker;
+import org.semanticweb.owlapi.expression.ShortFormEntityChecker;
 import org.semanticweb.owlapi.formats.FunctionalSyntaxDocumentFormat;
+import org.semanticweb.owlapi.manchestersyntax.renderer.ParserException;
 import org.semanticweb.owlapi.model.*;
-import org.semanticweb.owlapi.reasoner.InferenceType;
-import org.semanticweb.owlapi.reasoner.OWLReasoner;
-import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
+import org.semanticweb.owlapi.reasoner.*;
+import org.semanticweb.owlapi.util.BidirectionalShortFormProvider;
+import org.semanticweb.owlapi.util.BidirectionalShortFormProviderAdapter;
+import org.semanticweb.owlapi.util.ShortFormProvider;
+import org.semanticweb.owlapi.util.SimpleShortFormProvider;
 
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.*;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -21,9 +26,7 @@ public class Factory {
     private OWLReasoner mOWLReasoner;
     public void createOntology() throws OWLException {
       OWLOntologyManager man = OWLManager.createOWLOntologyManager();
-      OWLOntology o = man.createOntology();
-      System.out.println(o);
-
+      this.mOWLOntology = man.createOntology();
     }
     public void loadOntologyLocal(String nameOfOntology) throws OWLException {
         OWLOntologyManager man = OWLManager.createOWLOntologyManager();
@@ -71,23 +74,25 @@ public class Factory {
         mOWLOntology.addAxiom(domainAxiom);
         mOWLOntology.addAxiom(rangeAxiom);
     }
-    public void addComplexExpressionSome(String mAxiom, String mProperty){
+    public void addComplexExpressionSome(String mProperty, String mDomain, String mRange){
         OWLDataFactory df = mOWLOntology.getOWLOntologyManager().getOWLDataFactory();
-        OWLClass mOWLClass = df.getOWLClass(IRI.create("#" + mAxiom));
+        OWLClass mOWLClassDomain = df.getOWLClass(IRI.create("#" + mDomain));
+        OWLClass mOWLClassRange = df.getOWLClass(IRI.create("#" + mRange));
         OWLObjectProperty mOWLObjectProperty = df.getOWLObjectProperty(IRI.create("#" + mProperty));
         OWLClassAxiom ax = df.getOWLSubClassOfAxiom(
-                mOWLClass,
-                df.getOWLObjectSomeValuesFrom(mOWLObjectProperty, mOWLClass)
+                mOWLClassDomain,
+                df.getOWLObjectSomeValuesFrom(mOWLObjectProperty, mOWLClassRange)
         );
         mOWLOntology.addAxiom(ax);
     }
-    public void addComplexExpressionExactCardinality(String mAxiom, String mProperty, int cardinality){
+    public void addComplexExpressionExactCardinality(String mProperty, String mDomain, String mRange, int cardinality){
         OWLDataFactory df = mOWLOntology.getOWLOntologyManager().getOWLDataFactory();
-        OWLClass mOWLClass = df.getOWLClass(IRI.create("#" + mAxiom));
+        OWLClass mOWLClassDomain = df.getOWLClass(IRI.create("#" + mDomain));
+        OWLClass mOWLClassRange = df.getOWLClass(IRI.create("#" + mRange));
         OWLObjectProperty mOWLObjectProperty = df.getOWLObjectProperty(IRI.create("#" + mProperty));
         OWLClassAxiom ax = df.getOWLSubClassOfAxiom(
-                mOWLClass,
-                df.getOWLObjectExactCardinality(cardinality, mOWLObjectProperty, mOWLClass)
+                mOWLClassDomain,
+                df.getOWLObjectExactCardinality(cardinality, mOWLObjectProperty, mOWLClassRange)
         );
         mOWLOntology.addAxiom(ax);
     }
@@ -96,6 +101,17 @@ public class Factory {
         OWLNamedIndividual mOWLNamedIndividual = df.getOWLNamedIndividual(IRI.create("#" + mIndividual));
         OWLClass mOWLClass = df.getOWLClass( IRI.create("#" + mAxiom) );
         mOWLOntology.addAxiom(df.getOWLClassAssertionAxiom(mOWLClass, mOWLNamedIndividual));
+    }
+    public void addIndividualObjectProperty(String mIndividual, String mProperty, String mIndividualRange){
+        OWLDataFactory df = mOWLOntology.getOWLOntologyManager().getOWLDataFactory();
+        OWLNamedIndividual mOWLNamedIndividual = df.getOWLNamedIndividual(IRI.create("#" + mIndividual));
+        OWLNamedIndividual mOWLNamedIndividualRange = df.getOWLNamedIndividual(IRI.create("#" + mIndividualRange));
+        OWLObjectProperty mOWLObjectProperty = df.getOWLObjectProperty(IRI.create("#" + mProperty));
+        df.getOWLObjectHasValue(mOWLObjectProperty, mOWLNamedIndividual);
+        mOWLOntology.addAxiom(
+                df.getOWLObjectPropertyAssertionAxiom(mOWLObjectProperty, mOWLNamedIndividual, mOWLNamedIndividualRange)
+        );
+
     }
     public void removeAxiom(String mAxiom){
         OWLDataFactory df = mOWLOntology.getOWLOntologyManager().getOWLDataFactory();
@@ -129,5 +145,171 @@ public class Factory {
         return mOWLOntology;
     }
 
+    public void InitialiseQuery() throws IOException {
+        OWLReasoner reasoner = new Reasoner.ReasonerFactory().createReasoner(mOWLOntology);
+        ShortFormProvider shortFormProvider = new SimpleShortFormProvider();
+        // Create the DLQueryPrinter helper class. This will manage the
+        // parsing of input and printing of results
+        DLQueryPrinter dlQueryPrinter = new DLQueryPrinter(new DLQueryEngine(reasoner,
+                shortFormProvider), shortFormProvider);
+        // Enter the query loop. A user is expected to enter class
+        // expression on the command line.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
+        while (true) {
+            System.out
+                    .println("Type a class expression in Manchester Syntax and press Enter (or press x to exit):");
+            String classExpression = br.readLine();
+            // Check for exit condition
+            if (classExpression == null || classExpression.equalsIgnoreCase("x")) {
+                break;
+            }
+            dlQueryPrinter.askQuery(classExpression.trim());
+            System.out.println();
+        }
+    }
 
 }
+
+
+class DLQueryEngine {
+    private final OWLReasoner reasoner;
+    private final DLQueryParser parser;
+
+    public DLQueryEngine(OWLReasoner reasoner, ShortFormProvider shortFormProvider) {
+        this.reasoner = reasoner;
+        parser = new DLQueryParser(reasoner.getRootOntology(), shortFormProvider);
+    }
+
+    public Set<OWLClass> getSuperClasses(String classExpressionString, boolean direct) {
+        if (classExpressionString.trim().length() == 0) {
+            return Collections.emptySet();
+        }
+        OWLClassExpression classExpression = parser
+                .parseClassExpression(classExpressionString);
+        NodeSet<OWLClass> superClasses = reasoner
+                .getSuperClasses(classExpression, direct);
+        return superClasses.getFlattened();
+    }
+
+    public Set<OWLClass> getEquivalentClasses(String classExpressionString) {
+        if (classExpressionString.trim().length() == 0) {
+            return Collections.emptySet();
+        }
+        OWLClassExpression classExpression = parser
+                .parseClassExpression(classExpressionString);
+        Node<OWLClass> equivalentClasses = reasoner.getEquivalentClasses(classExpression);
+        Set<OWLClass> result = null;
+        if (classExpression.isAnonymous()) {
+            result = equivalentClasses.getEntities();
+        } else {
+            result = equivalentClasses.getEntitiesMinus(classExpression.asOWLClass());
+        }
+        return result;
+    }
+
+    public Set<OWLClass> getSubClasses(String classExpressionString, boolean direct) {
+        if (classExpressionString.trim().length() == 0) {
+            return Collections.emptySet();
+        }
+        OWLClassExpression classExpression = parser
+                .parseClassExpression(classExpressionString);
+        NodeSet<OWLClass> subClasses = reasoner.getSubClasses(classExpression, direct);
+        return subClasses.getFlattened();
+    }
+
+    public Set<OWLNamedIndividual> getInstances(String classExpressionString,
+                                                boolean direct) {
+        if (classExpressionString.trim().length() == 0) {
+            return Collections.emptySet();
+        }
+        OWLClassExpression classExpression = parser
+                .parseClassExpression(classExpressionString);
+        NodeSet<OWLNamedIndividual> individuals = reasoner.getInstances(classExpression,
+                direct);
+        return individuals.getFlattened();
+    }
+}
+
+class DLQueryParser {
+    private final OWLOntology rootOntology;
+    private final BidirectionalShortFormProvider bidiShortFormProvider;
+
+    public DLQueryParser(OWLOntology rootOntology, ShortFormProvider shortFormProvider) {
+        this.rootOntology = rootOntology;
+        OWLOntologyManager manager = rootOntology.getOWLOntologyManager();
+        Set<OWLOntology> importsClosure = rootOntology.getImportsClosure();
+        // Create a bidirectional short form provider to do the actual mapping.
+        // It will generate names using the input
+        // short form provider.
+        bidiShortFormProvider = new BidirectionalShortFormProviderAdapter(manager,
+                importsClosure, shortFormProvider);
+    }
+
+    public OWLClassExpression parseClassExpression(String classExpressionString) {
+        OWLDataFactory dataFactory = rootOntology.getOWLOntologyManager()
+                .getOWLDataFactory();
+        ManchesterOWLSyntaxEditorParser parser = new ManchesterOWLSyntaxEditorParser(
+                dataFactory, classExpressionString);
+        parser.setDefaultOntology(rootOntology);
+        OWLEntityChecker entityChecker = new ShortFormEntityChecker(bidiShortFormProvider);
+        parser.setOWLEntityChecker(entityChecker);
+        return parser.parseClassExpression();
+    }
+}
+
+class DLQueryPrinter {
+    private final DLQueryEngine dlQueryEngine;
+    private final ShortFormProvider shortFormProvider;
+
+    public DLQueryPrinter(DLQueryEngine engine, ShortFormProvider shortFormProvider) {
+        this.shortFormProvider = shortFormProvider;
+        dlQueryEngine = engine;
+    }
+
+    public void askQuery(String classExpression) {
+        if (classExpression.length() == 0) {
+            System.out.println("No class expression specified");
+        } else {
+            try {
+                StringBuilder sb = new StringBuilder();
+                sb.append("\nQUERY:   ").append(classExpression).append("\n\n");
+                Set<OWLClass> superClasses = dlQueryEngine.getSuperClasses(
+                        classExpression, false);
+                printEntities("SuperClasses", superClasses, sb);
+                Set<OWLClass> equivalentClasses = dlQueryEngine
+                        .getEquivalentClasses(classExpression);
+                printEntities("EquivalentClasses", equivalentClasses, sb);
+                Set<OWLClass> subClasses = dlQueryEngine.getSubClasses(classExpression,
+                        true);
+                printEntities("SubClasses", subClasses, sb);
+                Set<OWLNamedIndividual> individuals = dlQueryEngine.getInstances(
+                        classExpression, true);
+                printEntities("Instances", individuals, sb);
+                System.out.println(sb.toString());
+            } catch (ParserException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private void printEntities(String name, Set<? extends OWLEntity> entities,
+                               StringBuilder sb) {
+        sb.append(name);
+        int length = 50 - name.length();
+        for (int i = 0; i < length; i++) {
+            sb.append(".");
+        }
+        sb.append("\n\n");
+        if (!entities.isEmpty()) {
+            for (OWLEntity entity : entities) {
+                sb.append("\t").append(shortFormProvider.getShortForm(entity))
+                        .append("\n");
+            }
+        } else {
+            sb.append("\t[NONE]\n");
+        }
+        sb.append("\n");
+    }
+}
+
+
